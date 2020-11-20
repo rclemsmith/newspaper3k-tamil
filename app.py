0b9cca9741ec8f42d6d43cb7d8f32bc383bfc11d
Clement Smith <43886157+rclemsmith@users.noreply.github.com>
2020-11-20 16:19:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,6 @@ async def test_root():
     return "Hello from Newstuck Newspaper API. This API makes use of the python's Newspaper3K library to extract the main text content and the image source of the article, given the article's url. To retrieve the text and image, '/extract?url=<article_url_here>' "
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1',port="80",threaded=True)
+    app.run(host='0.0.0.0',port="80",threaded=True)
 
 
